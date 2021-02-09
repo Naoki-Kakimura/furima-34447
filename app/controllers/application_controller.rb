a01a3ffc578eeae7c39de3e5c57c035d698b366b
Naoki-Kakimura <kn1210zzr@outlook.jp>
2021-02-09 16:53:57 +0900

    バリエーションの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parmeters, if: :devise_controller?
+  private
+    def configure_permitted_parmeters
+      devise_parameter_sanitizer.permit(:sign_up, keys:[:name,:first_name_kan,:last_name_kan,:first_name_kata,:last_name_kata,:birthday])
+      devise_parameter_sanitizer.permit(:sign_in, keys:[:email,:password])
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :name, precense: true
-  validates :first_name_kan, precense: true
-  validates :last_name_kan, precense: true
-  validates :first_name_kata, precense: true
-  validates :last_name_kata, precense: true
-  validates :birthday, precense: true
+  validates :name, presence: true
+  validates :birthday, presence: true
+
+  with_options presence: true, format:{with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name_kan
+    validates :last_name_kan
+  end
+  
+  with_options presence: true, format:{with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください' }do
+  validates :first_name_kata
+  validates :last_name_kata
+  end
 end
