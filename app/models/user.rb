414f5156ccb61bdbe224f1840bcf81760f74d5f4
tyontyometyome1210 <kn1210zzr@outlook.jp>
2021-02-09 12:48:48 +0900

    Userモデルのバリエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, precense: true
+  validates :first_name_kan, precense: true
+  validates :last_name_kan, precense: true
+  validates :first_name_kata, precense: true
+  validates :last_name_kata, precense: true
+  validates :birthday, precense: true
 end
