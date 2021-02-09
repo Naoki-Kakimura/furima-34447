4f35ece5c11c6954b8199b385f7b21f535cfae52
Naoki-Kakimura <kn1210zzr@outlook.jp>
2021-02-09 18:37:51 +0900

    単体テスト以前まで実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parmeters, if: :devise_controller?
+
   private
     def configure_permitted_parmeters
-      devise_parameter_sanitizer.permit(:sign_up, keys:[:name,:first_name_kan,:last_name_kan,:first_name_kata,:last_name_kata,:birthday])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:first_name_kan,:last_name_kan,:first_name_kata,:last_name_kata,:birthday])
     end
+
 end
